0f198d6a84ffa8c6a6eae9c5a7e534e2b910b496
Felipe Mariano <86112326+felipemariano29@users.noreply.github.com>
2024-05-08 14:51:06 -0300

    Created register form

---
+++ src/app/dashboard/customer/components/form/index.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import {
+  RegisterOptions,
+  UseFormRegisterReturn,
+  useForm,
+} from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/input";
+
+const schema = z.object({
+  name: z.string().min(1, { message: "O nome é obrigatório" }),
+  email: z
+    .string()
+    .email({ message: "Digite um email válido" })
+    .min(1, { message: "O email é obrigatório" }),
+  phone: z.string().refine(
+    (value) => {
+      return /^(?:\(\d{2}\)\s?)?\d{9}$/.test(value);
+    },
+    {
+      message: "O telefone estar no formato: (11) 999999999",
+    }
+  ),
+  address: z.string(),
+});
+
+type FormData = z.infer<typeof schema>;
+
+export function NewCustomerForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  function handleRegister(data: FormData) {
+    console.log(data);
+  }
+
+  return (
+    <form
+      className="flex flex-col mt-6"
+      onSubmit={handleSubmit(handleRegister)}
+    >
+      <label className="mb-1 text-lg font-medium">Nome Completo: </label>
+      <Input
+        type="text"
+        name="name"
+        placeholder="Digite o nome completo"
+        error={errors.name?.message}
+        register={register}
+      />
+
+      <section className="flex gap-2 my-4 flex-col sm:flex-row">
+        <div className="flex-1">
+          <label className="mb-1 text-lg font-medium">Telefone: </label>
+          <Input
+            type="phone"
+            name="phone"
+            placeholder="Digite o telefone"
+            error={errors.phone?.message}
+            register={register}
+          />
+        </div>
+
+        <div className="flex-1">
+          <label className="mb-1 text-lg font-medium">Email: </label>
+          <Input
+            type="email"
+            name="email"
+            placeholder="Digite o email"
+            error={errors.email?.message}
+            register={register}
+          />
+        </div>
+      </section>
+
+      <label className="text-lg font-medium">Endereço: </label>
+      <Input
+        type="text"
+        name="address"
+        placeholder="Digite o endereço completo"
+        error={errors.address?.message}
+        register={register}
+      />
+
+      <button
+        type="submit"
+        className="bg-blue-500 hover:bg-blue-600 transition-colors my-4 px-2 h-11 rounded text-white font-bold "
+      >
+        Cadastrar
+      </button>
+    </form>
+  );
+}
--- src/app/dashboard/customer/new/page.tsx
+++ src/app/dashboard/customer/new/page.tsx
@@ -1,9 +1,34 @@
 import { Container } from "@/components/container";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import { FiArrowLeft } from "react-icons/fi";
+import { NewCustomerForm } from "../components/form";
 
-export default function NewCustomer() {
+export default async function NewCustomer() {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    redirect("/");
+  }
+
   return (
     <Container>
-      <h1>Novo cliente</h1>
+      <main className="flex flex-col mt-9 mb-2">
+        <div className="flex items-center gap-3">
+          <Link
+            href="/dashboard/customer"
+            className="bg-gray-900 p-2.5 rounded-full"
+          >
+            <FiArrowLeft color="#fff" />
+          </Link>
+
+          <h1 className="text-3xl font-bold">Novo cliente</h1>
+        </div>
+
+        <NewCustomerForm />
+      </main>
     </Container>
   );
 }
